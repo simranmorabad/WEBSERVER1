84fe36ea01a0fa3e032006529490860a5a4123a3
simranmorabad <simran.morabad@deeparmor.com>
2024-06-27 16:17:53 +0530

    Update loginandreg.mjs
    j

--- loginandreg.mjs
+++ loginandreg.mjs
@@ -157,7 +157,7 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server iss running on port ${port}`);
 });
 
 app.get('/protected-route', verifyToken, (req, res) => {
